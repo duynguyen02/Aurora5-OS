cb2036bc30ad4ab0cb5af3c0cf4f5d5340255e8c
Duy Nguyen <2051067158@e.tlu.edu.vn>
2022-09-24 14:37:14 +0700

    hoàn thành cd command

--- src/commands/cd/cd.c
+++ src/commands/cd/cd.c
@@ -3,11 +3,18 @@
 #include "stdlib.h"
 
 #include "../../libs/constants.h"
+#include "../../libs/utils/common.h"
 #include "../../libs/user/users_manager.h"
 
-int change_to_user_dir(char *rootPath)
+int change_to_user_dir(const char *rootPath)
 {
     UserInfo *user = get_current_user(rootPath);
+
+    if (user == NULL)
+    {
+        printf("Shell do not have any user!\n");
+        return EXIT_STATUS;
+    }
 
     if (strcmp(user->current_user, ADMIN_USER_NAME) == 0)
     {
@@ -25,9 +32,15 @@ int change_to_user_dir(char *rootPath)
     return SUCESS_EXIT_STATUS;
 }
 
-int change_to_root_dir(char *rootPath)
+int change_to_root_dir(const char *rootPath)
 {
     UserInfo *user = get_current_user(rootPath);
+
+    if (user == NULL)
+    {
+        printf("Shell do not have any user!\n");
+        return EXIT_STATUS;
+    }
 
     strcpy(user->current_dir, rootPath);
 
@@ -68,9 +81,15 @@ char *get_parent_dir(const char *path)
     return substr;
 }
 
-int change_to_parent_dir(char *rootPath)
+int change_to_parent_dir(const char *rootPath)
 {
     UserInfo *user = get_current_user(rootPath);
+
+    if (user == NULL)
+    {
+        printf("Shell do not have any user!\n");
+        return EXIT_STATUS;
+    }
 
     if (strcmp(user->current_dir, rootPath) != 0)
     {
@@ -89,6 +108,211 @@ int change_to_parent_dir(char *rootPath)
     }
 
     return SUCESS_EXIT_STATUS;
+}
+
+int change_to_dir(char *rootPath, char *path)
+{
+    char *final_path = calloc(MAX_BUFFER_SIZE, sizeof(char));
+    UserInfo *user = get_current_user(rootPath);
+    if (user == NULL)
+    {
+        printf("Shell do not have any user!\n");
+        return EXIT_STATUS;
+    }
+
+    // nếu ký tự đầu tiền là '.' cd sẽ thực hiện đến các thư mục con của thư mục hiện tại
+    if (path[0] == '.')
+    {
+        strcpy(final_path, user->current_dir);
+
+        char *temp_path = calloc(MAX_BUFFER_SIZE, sizeof(char));
+        strcpy(temp_path, path);
+
+        if (path[1] == '/')
+        {
+            // nếu sau kí tự / còn ký tự tức là user muốn vào thư mục con
+            if (path[2] != '\0')
+            {
+                char *start = &temp_path[1];
+                char *end = &temp_path[strlen(temp_path)];
+
+                char *substr = (char *)calloc(1, end - start + 1);
+
+                memcpy(substr, start, end - start);
+
+                strcat(final_path, substr);
+            }
+
+            // nếu thư mục không tồn tại thì báo lỗi
+            if (is_dir_exists(final_path) == 0)
+            {
+                printf("cd: no such file or directory\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            strcpy(user->current_dir, final_path);
+
+            if (replace_the_last_shell_user(rootPath, *user) == 0)
+            {
+                printf("Something went wrong, try again!\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            return SUCESS_EXIT_STATUS;
+        }
+        else
+        {
+            strcat(final_path, "/");
+
+            char *start = &temp_path[0];
+            char *end = &temp_path[strlen(temp_path)];
+
+            char *substr = (char *)calloc(1, end - start + 1);
+
+            memcpy(substr, start, end - start);
+
+            strcat(final_path, substr);
+
+            // nếu thư mục không tồn tại thì báo lỗi
+            if (is_dir_exists(final_path) == 0)
+            {
+                printf("cd: no such file or directory\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            strcpy(user->current_dir, final_path);
+
+            if (replace_the_last_shell_user(rootPath, *user) == 0)
+            {
+                printf("Something went wrong, try again!\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            return SUCESS_EXIT_STATUS;
+        }
+    }
+    // nếu ký tự đầu tiền là '~' cd sẽ thực hiện đến các thư mục con của thư mục người dùng
+    else if (path[0] == '~')
+    {
+        strcpy(final_path, get_user_dir(rootPath, user->current_user));
+
+        if (path[1] == '/')
+        {
+            // nếu sau kí tự / không còn kí tự sẽ chuyển về thư mục người dùng.
+            if (path[2] == '\0')
+            {
+                return change_to_user_dir(rootPath);
+            }
+
+            char *temp_path = calloc(MAX_BUFFER_SIZE, sizeof(char));
+            strcpy(temp_path, path);
+
+            strcat(final_path, "/");
+
+            char *start = &temp_path[2];
+            char *end = &temp_path[strlen(temp_path)];
+
+            char *substr = (char *)calloc(1, end - start + 1);
+
+            memcpy(substr, start, end - start);
+
+            strcat(final_path, substr);
+
+            // nếu thư mục không tồn tại thì báo lỗi
+            if (is_dir_exists(final_path) == 0)
+            {
+                printf("cd: no such file or directory\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            strcpy(user->current_dir, final_path);
+
+            if (replace_the_last_shell_user(rootPath, *user) == 0)
+            {
+                printf("Something went wrong, try again!\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            return SUCESS_EXIT_STATUS;
+        }
+        else
+        {
+            printf("Error location, try again!\n");
+            return ERROR_EXIT_STATUS;
+        }
+    }
+
+    // nếu ký tự đầu tiền là '/' cd sẽ thực hiện đến các thư mục con của thư mục root
+    else if (path[0] == '/')
+    {
+        strcpy(final_path, rootPath);
+
+        if (path[1] != '/')
+        {
+
+            char *temp_path = calloc(MAX_BUFFER_SIZE, sizeof(char));
+            strcpy(temp_path, path);
+
+            strcat(final_path, "/");
+
+            char *start = &temp_path[1];
+            char *end = &temp_path[strlen(temp_path)];
+
+            char *substr = (char *)calloc(1, end - start + 1);
+
+            memcpy(substr, start, end - start);
+
+            strcat(final_path, substr);
+
+            // nếu thư mục không tồn tại thì báo lỗi
+            if (is_dir_exists(final_path) == 0)
+            {
+                printf("cd: no such file or directory\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            strcpy(user->current_dir, final_path);
+
+            if (replace_the_last_shell_user(rootPath, *user) == 0)
+            {
+                printf("Something went wrong, try again!\n");
+                return ERROR_EXIT_STATUS;
+            }
+
+            return SUCESS_EXIT_STATUS;
+        }
+        else
+        {
+            printf("Error location, try again!\n");
+            return ERROR_EXIT_STATUS;
+        }
+    }
+    else
+    {
+        strcpy(final_path, user->current_dir);
+        strcat(final_path, "/");
+        strcat(final_path, path);
+
+        // nếu thư mục không tồn tại thì báo lỗi
+        if (is_dir_exists(final_path) == 0)
+        {
+            printf("cd: no such file or directory\n");
+            return ERROR_EXIT_STATUS;
+        }
+
+        strcpy(user->current_dir, final_path);
+
+        if (replace_the_last_shell_user(rootPath, *user) == 0)
+        {
+            printf("Something went wrong, try again!\n");
+            return ERROR_EXIT_STATUS;
+        }
+
+        return SUCESS_EXIT_STATUS;
+    }
+
+    printf("Something went wrong, try again!\n");
+    return ERROR_EXIT_STATUS;
 }
 
 int main(int argc, char **argv)
@@ -117,22 +341,17 @@ int main(int argc, char **argv)
         {
             return change_to_parent_dir(argv[argc - 1]);
         }
+        else if (strcmp(argv[1], ".") == 0)
+        {
+            return SUCESS_EXIT_STATUS;
+        }
+        else
+        {
+            return change_to_dir(argv[argc - 1], argv[1]);
+        }
     }
 
-    // if (argc == 5)
-    // {
-    //     UserInfo *user = get_current_user(argv[4]);
-
-    //     if (user->is_admin != 1)
-    //     {
-    //         printf("You are not root user to use this command!\n");
-    //         return 2;
-    //     }
-
-    //     return try_to_add_user(argv[1], argv[2], argv[3], argv[4]);
-    // }
-
-    printf("useradd: Error: to many arguments, try again!\n");
+    printf("cd: Error: too many or few arguments, try again!\n");
 
     return ERROR_EXIT_STATUS;
 }
